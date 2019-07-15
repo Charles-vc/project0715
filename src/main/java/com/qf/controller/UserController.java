1fb909ad694fe7f57d3045fb6fd78e2935495f97
VcZint <VcZint@163.com>
2019-07-15 20:16:01 +0800

    test1133

---
+++ src/main/java/com/qf/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("f")
+    public String test(){
+
+        return "login";
+    }
+
+    @RequestMapping("/unauthorize")
+    public String test2(){
+
+        return "unauthorize";
+    }
+    @RequestMapping("/test2")
+    public String test3(){
+
+        return "success";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/unauthorize.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: asus
+  Date: 2019/7/15
+  Time: 19:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>没权限
+</body>
+</html>
