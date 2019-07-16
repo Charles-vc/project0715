51dab4421bd6e9eae5f435004fc5d1f3c1606461
zedking <1058972165@qq,com>
2019-07-16 17:35:45 +0800

    realm

--- src/main/java/com/qf/realm/UserRealm.java
+++ src/main/java/com/qf/realm/UserRealm.java
@@ -1,20 +1,30 @@
 package com.qf.realm;
 
+import com.qf.service.UserService;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRealm extends AuthorizingRealm {
+    @Autowired
+    private UserService userService;
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         return null;
+
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        return null;
+        String uname = (String) authenticationToken.getPrincipal();
+        String password = userService.getPassword(uname);
+        System.out.println(password);
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(uname,password,"UserRealm");
+        return  simpleAuthenticationInfo;
     }
 }
