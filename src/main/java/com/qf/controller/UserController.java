46deec5ea0c466653e7c14269ba6ed7862c33c71
zedking <1058972165@qq,com>
2019-07-16 15:35:41 +0800

    controller

--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -1,24 +1,53 @@
 package com.qf.controller;
 
+import com.qf.pojo.User;
+import com.qf.service.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class UserController {
+    @Autowired
+    private SecurityManager securityManager;
+    @Autowired
+    private UserService userService;
 
-    @RequestMapping("f")
+    @RequestMapping("/first")
     public String test(){
-
         return "login";
     }
     @RequestMapping("Home")
-    public String test5(){
-
+    public String home(){
         return "Home";
     }
+    @RequestMapping("login")
+    public String login(User user, HttpSession session){
+        System.out.println(user.getUname());
+        System.out.println(user.getPassword());
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(user.getUname(),user.getPassword());
+        try{
+            subject.login(usernamePasswordToken);
+            if(subject.isAuthenticated()){
+                int uid = userService.getUid(user.getUname());
+                session.setAttribute("uid",uid);
+                return "Home";
+            }
+        }catch (SecurityException e){
+            System.out.println("登录失败");
+        }
+        return "login";
 
 
-
+    }
     @RequestMapping("/unauthorize")
     public String test2(){
 
